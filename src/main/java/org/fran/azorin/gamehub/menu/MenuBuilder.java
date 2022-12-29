a180d864e88c4ca8f9c86332b392f8e084434524
Fran Azorin <40206025+Pakychoko@users.noreply.github.com>
2022-12-29 17:37:57 +0100

    v1.2 main menu added

---
+++ src/main/java/org/fran/azorin/gamehub/menu/MenuBuilder.java
@@ -0,0 +1,39 @@
+package org.fran.azorin.gamehub.menu;
+
+public class MenuBuilder {
+
+    private Menu menu;
+
+    private MenuBuilder(final Menu menu) {
+        this.menu = menu;
+    }
+
+
+    public static MenuBuilder createMainMenu(final String title) {
+        return new MenuBuilder(new Menu(title));
+    }
+
+    public static MenuBuilder createSubMenu(final String title, final Integer selection) {
+        return new MenuBuilder(new Menu(title, selection));
+    }
+
+    public MenuBuilder addSimpleOption(final Integer selection, final String text) {
+        this.menu.addMenuOption(new MenuOption(selection, text, false));
+        return this;
+    }
+
+    public MenuBuilder addExitOption(final Integer selection, final String text) {
+        this.menu.addMenuOption(new MenuOption(selection, text, true));
+        return this;
+    }
+
+    public MenuBuilder addSubMenuOption(final String text, final Menu subMenu) {
+        this.menu.addMenuOption(new MenuOption(subMenu.getSelection(), text, false));
+        this.menu.addSubMenu(subMenu);
+        return this;
+    }
+
+    public Menu build() {
+        return this.menu;
+    }
+}
